7f4025e034abab574df7697812bdad6ca5713b59
sultanemiluulu <sultanchoponov@gmail.com>
2019-04-17 21:02:21 +0600

    БД переделана с sqlite3 на mysql в settings.py

--- refactor_oc/refactor_oc/settings.py
+++ refactor_oc/refactor_oc/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'refactor_oc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
     }
 }
 
@@ -120,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
