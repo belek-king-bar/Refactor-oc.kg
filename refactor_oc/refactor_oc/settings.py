f504e7fabc02a8073325bdd111ca43e62468d5e5
mrty <za.slonikov@gmail.com>
2019-04-17 16:27:44 +0600

    #2 БД перделана в settings.py на mysql

--- refactor_oc/refactor_oc/settings.py
+++ refactor_oc/refactor_oc/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'refactor_oc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
