4f0b4f11b518f49d62654a1c0dc3c26c2eeeb464
Daria <mamontova.dasha@gmail.com>
2019-04-18 20:22:59 +0600

    #9 Создано фейковое представление для страницы с информацией о фильме

--- refactor_oc/webapp/views.py
+++ refactor_oc/webapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+class MovieDetailView(DetailView):
+    template_name = 'movie_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'pk': '1',
+            'name': 'Name',
+            'description': 'Description',
+            'year': 'Year',
+            'genres': ['genre1', 'genre2', 'genre3']
+
+        }
+        return render(request, 'movie_detail.html', context)
