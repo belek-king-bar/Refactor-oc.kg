5fbdeac6d737f0e3b7dab071672c22a5b2febd91
sultanemiluulu <sultanchoponov@gmail.com>
2019-04-29 20:55:01 +0600

    #11 Добавлено представление для главной страницы Bestseller

--- refactor_oc/webapp/urls.py
+++ refactor_oc/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views import MovieDetailView, ActorDetailView, FavoritesListView
+from webapp.views import MovieDetailView, BestsellerListView, FavoritesListView, ActorDetailView
 
 app_name = 'webapp'
 
 urlpatterns = [
+    path('', BestsellerListView.as_view(), name='bestseller_list'),
     path('favorites/', FavoritesListView.as_view(), name='favorites_list'),
     path('movie/<int:pk>', MovieDetailView.as_view(), name='movie_detail'),
     path('actor/<int:pk>', ActorDetailView.as_view(), name='actor_detail'),
--- refactor_oc/webapp/views.py
+++ refactor_oc/webapp/views.py
@@ -1,11 +1,11 @@
-from webapp.models import Person, Bookmark
+from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import auth
+from webapp.models import Bestseller, Movie, Person, Bookmark
+import json
+
 
 class MovieDetailView(DetailView):
     template_name = 'movie_detail.html'
-
 
     def get(self, request, *args, **kwargs):
         context = {
@@ -34,3 +34,36 @@ class FavoritesListView(ListView):
         else:
             return
         pass #todo сделать редирект(?) на главную/логинку
+
+
+class BestsellerListView(ListView):
+    model = Bestseller
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwards):
+        context = super().get_context_data(**kwards)
+        context['movie_all'] = []
+        classes = {
+            1: 'fa-angellist',
+            2: 'fa-smile-o',
+            3: 'fa-bomb',
+            4: 'fa-video-camera',
+            5: 'fa-cloud',
+            6: 'fa-bug',
+            7: 'fa-meh-o',
+            8: 'fa-magic',
+            9: 'fa-heart',
+            10: 'fa-user-secret'
+        }
+        for bestseller in Bestseller.objects.all():
+            movies_str = bestseller.movies
+            movie_list = json.loads(movies_str)
+            q = {
+                'category': bestseller.name,
+                'movies': [],
+                'classname': classes[bestseller.category_id]
+            }
+            for i in movie_list:
+                q['movies'].append(Movie.objects.get(movie_id=i))
+            context['movie_all'].append(q)
+        return context
