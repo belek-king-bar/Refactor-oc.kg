249c5099f66ee11d5ff14ef65664dca953b62482
sultanemiluulu <sultanchoponov@gmail.com>
2019-04-29 20:58:10 +0600

    #11 Реализован юниттест для BestsellerView проверяющий структуру контекста, код запроса представления

---
+++ refactor_oc/webapp/tests/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.test import Client
+from django.core.management import call_command
+from django.conf import settings
+from django.urls import reverse
+
+
+class BestsellerViewTests(TestCase):
+    def setUp(self):
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/movies', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/bestsellers', verbosity=0)
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get(reverse('webapp:bestseller_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['movie_all']), 10)
+        self.assertEqual(type(response.context['movie_all']), list)
+        for i in response.context['movie_all']:
+            self.assertEqual(type(i), dict)
+        self.assertEqual(type(response.context['movie_all'][0]['category']), str)
+        self.assertEqual(type(response.context['movie_all'][0]['movies']), list)
+
