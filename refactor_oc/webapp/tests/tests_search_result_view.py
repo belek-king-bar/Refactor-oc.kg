59c63b78ed34e4a9d127d6b8a256a72a9cfc4bb0
pavel <zhpv07@gmail.com>
2019-05-09 19:48:43 -0400

    #21 Добавлен unit test для страница результатов поиска.

---
+++ refactor_oc/webapp/tests/tests_search_result_view.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.test import Client
+from django.core.management import call_command
+from django.conf import settings
+from django.urls import reverse
+
+
+class SearchViewTests(TestCase):
+    def setUp(self):
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/auth_user', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/user', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/comments', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/genres', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/movies', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/persons', verbosity=0)
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get(reverse('webapp:search_list'), {'q': 'Тачки'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('movies' in response.context)
+        self.assertTrue('persons' in response.context)
+        self.assertTrue(len(response.context['movies']) <= 6)
+        self.assertTrue(len(response.context['persons']) <= 6)
+        for i in response.context['persons']:
+            self.assertEqual(type(i.name), str)
+            self.assertEqual(type(i.international_name), str)
+        for i in response.context['movies']:
+            self.assertEqual(type(i.name), str)
+            self.assertEqual(type(i.international_name), str)
