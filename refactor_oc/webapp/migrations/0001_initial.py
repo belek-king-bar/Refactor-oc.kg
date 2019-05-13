490e5e870f8389ed273d8df0affb4f573fff0473
sultanemiluulu <sultanchoponov@gmail.com>
2019-05-13 19:26:35 +0600

    #32 Изменена модель OCUser

--- refactor_oc/webapp/migrations/0001_initial.py
+++ refactor_oc/webapp/migrations/0001_initial.py
@@ -10,10 +10,43 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0011_update_proxy_permissions'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='OCUser',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('first_name', models.CharField(blank=True, max_length=30, verbose_name='first name')),
+                ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('login', models.CharField(max_length=32, unique=True)),
+                ('password', models.CharField(max_length=255)),
+                ('email', models.CharField(max_length=255, unique=True)),
+                ('ip', models.TextField()),
+                ('balans', models.DecimalField(decimal_places=2, default=1.0, max_digits=9)),
+                ('user_group', models.SmallIntegerField(default=0)),
+                ('view_activity', models.IntegerField(null=True)),
+                ('play_activity', models.IntegerField(null=True)),
+                ('register_date', models.DateTimeField(auto_now_add=True)),
+                ('mode', models.IntegerField(default=1, null=True)),
+                ('ipfw_rule', models.IntegerField(default=0, null=True)),
+                ('enabled', models.SmallIntegerField(default=1, null=True)),
+                ('preferences', models.TextField(default=None, null=True)),
+                ('status', models.CharField(default=None, max_length=2048, null=True)),
+                ('username', models.CharField(blank=True, max_length=200, null=True)),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions')),
+            ],
+            options={
+                'db_table': 'users',
+            },
+        ),
         migrations.CreateModel(
             name='Bestseller',
             fields=[
@@ -334,35 +367,13 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='OCUser',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('email', models.CharField(max_length=255)),
-                ('ip', models.TextField()),
-                ('balans', models.DecimalField(decimal_places=2, default=1.0, max_digits=9)),
-                ('user_group', models.SmallIntegerField(default=0)),
-                ('view_activity', models.IntegerField()),
-                ('play_activity', models.IntegerField()),
-                ('register_date', models.DateTimeField(default=None)),
-                ('mode', models.IntegerField(default=1)),
-                ('ipfw_rule', models.IntegerField(default=0)),
-                ('enabled', models.SmallIntegerField(default=1)),
-                ('preferences', models.TextField(default=None)),
-                ('status', models.CharField(default=None, max_length=2048)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='OC', to=settings.AUTH_USER_MODEL, verbose_name='User')),
-            ],
-            options={
-                'db_table': 'users',
-            },
-        ),
-        migrations.CreateModel(
             name='MovieUserRating',
             fields=[
                 ('movie_user_rating_id', models.AutoField(primary_key=True, serialize=False)),
                 ('rating', models.SmallIntegerField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('movie', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='user_ratings', to='webapp.Movie')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='user_ratings', to='webapp.OCUser')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='user_ratings', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'movies_users_ratings',
@@ -380,10 +391,33 @@ class Migration(migrations.Migration):
                 ('ip', models.CharField(blank=True, max_length=15, null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('movie', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='hits', to='webapp.Movie')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='hits', to='webapp.OCUser')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='hits', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'hits',
+            },
+        ),
+        migrations.CreateModel(
+            name='CommentsVote',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('votes', models.IntegerField(blank=True, null=True)),
+                ('comments', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments_votes', to='webapp.Comment')),
+                ('users', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='comments_votes', to='webapp.OCUser')),
+            ],
+            options={
+                'db_table': 'comments_votes',
+            },
+        ),
+        migrations.CreateModel(
+            name='CommentsRating',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('vote', models.IntegerField(blank=True, null=True)),
+                ('comment', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='comments_ratings', to='webapp.Comment')),
+            ],
+            options={
+                'db_table': 'comments_rating',
             },
         ),
         migrations.AddField(
@@ -401,7 +435,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('bookmark_id', models.AutoField(primary_key=True, serialize=False)),
                 ('movie', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='bookmarks', to='webapp.Movie')),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.PROTECT, related_name='bookmarks', to='webapp.OCUser')),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.PROTECT, related_name='bookmarks', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'bookmarks',
@@ -412,7 +446,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('comment', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, primary_key=True, related_name='comments_votes', serialize=False, to='webapp.Comment')),
                 ('votes', models.IntegerField(blank=True, null=True)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='comments_votes', to='webapp.OCUser')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='comments_votes', to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'comments_votes',
