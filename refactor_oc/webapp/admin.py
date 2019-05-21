c3cc75a24a7182436c6665b71eb85003cfe8ad1a
sultanemiluulu <sultanchoponov@gmail.com>
2019-05-21 19:07:46 +0600

    #51 Добавлены все модели в админку

--- refactor_oc/webapp/admin.py
+++ refactor_oc/webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from webapp.models import *
 
+models = OCUser, Movie, Bestseller, Bookmark, Check, Comment, CommentsVote, CommentsRating, Incoming, Country, File, Filesstat, FilesLost, FilesTask, Genre, Hit, Log, Participant, Person, Rating, Registry, Role, SearchTrigram, Selection, Suggestion, SuggestionCache
+
+
+admin.site.register(models)
 
