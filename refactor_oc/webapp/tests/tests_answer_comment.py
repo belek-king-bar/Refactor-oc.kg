8d139de7f4385d9fecc4edd472373d3269dfe6c4
sultanemiluulu <sultanchoponov@gmail.com>
2019-06-18 17:49:20 +0600

    #59 Юнит тест для проверки ответов на комментарии

---
+++ refactor_oc/webapp/tests/tests_answer_comment.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.test import Client
+from django.core.management import call_command
+from django.conf import settings
+from django.urls import reverse
+import json
+
+
+class CommentAddTests(TestCase):
+    def setUp(self):
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/users', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/comments', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/genres', verbosity=0)
+        call_command('loaddata', settings.BASE_DIR + '/webapp/tests/fixtures/movies', verbosity=0)
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.post(reverse('webapp:answer_comment'),
+                                    {'movie_id': 4, 'text': 'Хороший фильм', 'user_pk': 1, 'to_comment_id': 5})
+        self.assertEqual(response.status_code, 200)
+        response = json.loads(response.content)
+        self.assertEqual(type(response), list)
+        self.assertTrue('user' in response[0])
+        self.assertTrue('text' in response[0])
+        self.assertTrue('created_at' in response[0])
+        self.assertTrue('to_comment_id' in response[0])
+
